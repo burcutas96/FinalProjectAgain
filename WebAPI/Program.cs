0b4e498db9064c1cee47f6a989adbe59af013b49
Burcu Taş <burcutas636@gmail.com>
2023-01-30 21:46:19 +0300

    Autofac vehicle was added

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,18 +1,30 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependencyResolvers.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+    .ConfigureContainer<ContainerBuilder>(builder =>
+    {
+        builder.RegisterModule(new AutofacBusinessModule());
+    });
 // Add services to the container.
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<IProductService, ProductManager>();  //AddSingleton, tüm bellekte sadece bir tane
-builder.Services.AddSingleton<IProductDal, EfProductDal>();        //ProductManager yani örnek oluþturuyor.
+
+
+
+//builder.Services.AddSingleton<IProductService, ProductManager>();  //AddSingleton, tüm bellekte sadece bir tane
+//builder.Services.AddSingleton<IProductDal, EfProductDal>();        //ProductManager yani örnek oluþturuyor.
                                                                    //Bütün isteklere ayný, tek bir instence'ý veriyor.
 
 var app = builder.Build();
