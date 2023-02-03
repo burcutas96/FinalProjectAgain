be0dcc053354ebd5fe3849e64801fb847094bd21
Burcu Taş <burcutas636@gmail.com>
2023-02-03 14:53:51 +0300

    Aop and FluentValidation support were added

---
+++ Core/CrossCuttingConcerns/Validation/ValidationTool.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Validation
+{
+    public static class ValidationTool
+    {                                    //entity'i object tipinde tutuyorum çünkü entity'e her şey gelebilir. Dto'lar mesela
+        public static void Validate(IValidator validator, object entity)
+        {
+            var context = new ValidationContext<object>(entity);  //Product için doğrulama contexti oluşturuldu.
+            var result = validator.Validate(context);   //productValidator'daki kurallara göre nesneyi kontrol et, doğrula.
+            if (!result.IsValid) //Eğer sonuç geçerli değilse hata fırlat. 
+            {
+                throw new ValidationException(result.Errors);
+            }
+        }
+    }
+}
